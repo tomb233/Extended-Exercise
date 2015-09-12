70ba9c14a5e33b8b1fc38d24c0af3659e83c6c5f
Thomas Buckle <thomasregan@me.com>
2015-09-12 18:05:22 +0100

    Version 2- Added a new method to find answer, after learning of new way to achieve same result

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,7 @@ public class Main {
     static int counter = 1;
     static int input1;
     static int input2;
-    static int difference;
-    static int smallest;
-    static int largest;
+
 
     public static void getInput() {
         Scanner kboard = new Scanner(System.in);
@@ -19,33 +17,7 @@ public class Main {
         input2 = kboard.nextInt();
         System.out.println("You chose " + input1 + " and " + input2);
     }
-    public static void evaluate(int x,int y){
 
-        if(x > y){ // x is greater than y
-            largest = x;
-            smallest = y;
-
-        }else if (x < y){ // x is less than y
-            largest = y;
-            smallest = x;
-
-
-        }else { // x must be equal to y
-
-            foundAnswer();
-        }
-        
-    } //Evaluates the two numbers and decides which is the smallest or if they are equal to eachother
-
-    public static void findDifference (int x, int y){
-        difference = Math.abs(x-y);
-    }//Finds the difference between both numbers submitted by the user.
-
-    public static int replaceNumbers(){
-        input1 = smallest;
-        input2 = difference;
-        return difference;
-    }
 
     public static void foundAnswer(){
         System.out.println("Answer Found!");
@@ -53,34 +25,19 @@ public class Main {
         System.exit(0);
     }
 
-    public static void counter(){
 
-        if(counter > 999999){
-            System.out.println("Fatal error!! Exiting before system crash");
-            System.exit(-1);
+
+
+
+    static int gcd(int x, int y)
+    {
+        while(x!=0 && y!=0)
+        {
+            int z = y;
+            y = x%y;
+            x = z;
         }
-    }
-
-    public static int gcd(int i1,int i2) {
-
-        evaluate(i1,i2);
-        findDifference(i1,i2);
-        int x = replaceNumbers();
-        return x;
-    }
-    public static int gcdTest(int i1,int i2){
-        int c = 1;
-        int x = 0;
-        while(c == 1){
-            evaluate(i1,i2);
-            findDifference(i1,i2);
-             x = replaceNumbers();
-            if(input1 == difference || input2 == difference){
-                c = 2;
-            }
-        }
-
-        return x;
+        return x+y;
     }
 
     public static void test(){
@@ -102,8 +59,8 @@ public class Main {
             input2 = array2[x];
 
 
-            answer  = gcdTest(input1,input2);
-            System.out.println(answer);
+            answer = gcd(input1,input2);
+
 
             System.out.println("Test "+(x+1)+": gcd("+array1[x]+", "+array2[x]+") = " + answer);
             if(answer == array3[x]){
@@ -129,16 +86,12 @@ public class Main {
             int x = 1;
 
             getInput();
-            while (x == 1){
-                counter();
-                System.out.println("Currently on pass "+ counter);
-                int y = gcd(input1,input2);
-                System.out.println("Largest common denominator currently is "+ y);
-                counter++;
+            input1 = gcd(input1,input2);
+            foundAnswer();
 
             }
         }
 
 
     }
-}
+
